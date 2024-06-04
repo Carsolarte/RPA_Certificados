46b5aa0778ec986bd182d3b155c7c729db08673c
Carsolarte <carsolarte@unicauca.edu.co>
2024-06-04 18:38:36 -0500

    rpa certificados terminado

--- certificados.py
+++ certificados.py
@@ -3,40 +3,46 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import base64
 import time
 import requests
 from anticaptchaofficial.imagecaptcha import *
 
-
+#Configuracion del navegador 
 chrome_options=Options()
 chrome_options.add_experimental_option("detach", True)
 driver=webdriver.Chrome(options=chrome_options)
-    
 print("Navegador abierto")
+
 driver.maximize_window()
+
 driver.get("https://www.adres.gov.co/consulte-su-eps")
 print("Página cargada")
 driver.implicitly_wait(2)
 
+#Se cambia el contexto al iframe que contiene el formulario
 iframe= driver.find_element(By.XPATH,'//*[@id="MSOPageViewerWebPart_WebPartWPQ3"]')
 driver.switch_to.frame(iframe)
 
+#Seleccion del tipo de documento de la lista desplegable
 select= driver.find_element(By.XPATH,'//*[@id="tipoDoc"]')
 tipo_doc_op=select.find_elements(By.TAG_NAME,'option')
-
 seleccionar= Select( driver.find_element(By.XPATH,'//*[@id="tipoDoc"]'))
 seleccionar.select_by_value("CC")
 
-
+#Ingreso del numero de docuento
 num_doc= driver.find_element(By.XPATH,'//*[@id="txtNumDoc"]')
 num_doc.send_keys('1004235043')
+
+#Resolver el captcha
 captchaBool = False
 while captchaBool is not True:
     try:
         # Capturar la imagen del captcha
       captchaImage = driver.find_element(By.XPATH, '//*[@id="Capcha_CaptchaImageUP"]')
-
+      #Script de javascript 
       captchaImageSave = driver.execute_async_script("""
           var ele = arguments[0], callback = arguments[1];
           ele.addEventListener('load', function fn(){
@@ -72,3 +78,24 @@ while captchaBool is not True:
     except Exception as e:
       print(f"Volviendo a intentar el captcha debido a: {e}")
       time.sleep(2) 
+
+
+# Esperar la apertura de una nueva pestaña
+WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(2))
+
+# Cambiar al nuevo handle de la pestaña
+for handle in driver.window_handles:
+    if handle != driver.current_window_handle:
+        driver.switch_to.window(handle)
+        break
+    
+time.sleep(5)
+
+# Extraer el HTML de la página
+page_html = driver.page_source
+
+# Guardar el HTML en un archivo
+with open("resultado.html", "w", encoding='utf-8') as file:
+    file.write(page_html)
+
+print("HTML de la página guardado en resultado.html")
